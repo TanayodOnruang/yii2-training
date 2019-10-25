e31b45b0d6e55c0427eccc7fdb9f2ac8bd9e958f
Konicaminolta <Konicaminolta@172.16.48.53>
2019-10-25 12:24:04 +0700

    อบรมเชิงปฏิบัติการ

---
+++ frontend/views/site/index.php
@@ -0,0 +1,18 @@
+<?php
+
+/* @var $this yii\web\View */
+
+$this->title = 'Yii2-Training@�?ัทลุ�?';
+?>
+<div class="site-index">
+
+    <div class="jumbotron">
+        <h2>สำ�?ั�?�?า�?สา�?ารณสุ�? �?ั�?หวัด�?ัทลุ�?123456กข</h2>
+
+        <p class="lead text-danger">อ�?รมเ�?ิ�?�?�?ิ�?ัติ�?าร �?ารเ�?ีย�?�?�?ร�?�?รมด�?วยภาษา PHP ด�?วย Yii Framwork 2</p>
+        <h4>ระหว�?า�?วั�?ที�? 24-25 ตุลา�?ม 2562</h4>
+
+        <p><a class="btn btn-warning btn-sm" href="http://www2.ptho.moph.go.th/" target="_blank"> ยิ�?ดีต�?อ�?รั�?สู�? สส�?.�?ัทลุ�?</a></p>
+    </div>
+
+</div>
